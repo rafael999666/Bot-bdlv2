c4c65945b7ff388ed89df7ecc619e91bda1e05c1
BARDOKILL <76783124+rafael999666@users.noreply.github.com>
2021-01-08 09:38:12 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,98 @@
+
+const help = (prefix) => { 
+	return `                 
+┏━━❉ *Sobre o bot* ❉━━┓
+┣⊱ *${prefix}owner*
+┣⊱ *${prefix}donasi*
+┣⊱ *${prefix}info*
+┣⊱ *${prefix}botstat*
+┣⊱ *${prefix}bug*
+┣━━❀ *Maker* ❀━━
+┣⊱ *${prefix}ocr*
+┣⊱ *${prefix}sticker*
+┣⊱ *${prefix}tts*
+┣⊱ *${prefix}toimg*
+┣⊱ *${prefix}phlogo*
+┣⊱ *${prefix}quotemaker*
+┣⊱ *${prefix}ninjalogo*
+┣⊱ *${prefix}glitch*
+┣⊱ *${prefix}text3d*
+┣━━❀ *Magic Shell* ❀━
+┣⊱ *${prefix}kapankah*
+┣⊱ *${prefix}apakah*
+┣⊱ *${prefix}bisakah*
+┣⊱ *${prefix}rate*
+┣━━❀ *Diversão* ❀━━━━
+┣⊱ *${prefix}meme*
+┣⊱ *${prefix}memeindo*
+┣⊱ *${prefix}hilih*
+┣⊱ *${prefix}fitnah*
+┣⊱ *${prefix}truth*
+┣⊱ *${prefix}dare*
+┣⊱ *${prefix}bucin*
+┣⊱ *${prefix}persengay*
+┣━━❀ *Anime* ❀━━
+┣⊱ *${prefix}loli*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}anime*
+┣⊱ *${prefix}randomanime*
+┣⊱ *${prefix}randomhentai*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwblowjob*
+┣⊱ *${prefix}nsfwneko*
+┣⊱ *${prefix}nsfwtrap*
+┣━━❀ *Media* ❀━━
+┣⊱ *${prefix}tiktokstalk*
+┣⊱ *${prefix}ytmp4*
+┣━━❀ *Outros* ❀━━
+┣⊱ *${prefix}ping*
+┣⊱ *${prefix}shorturl*
+┣⊱ *${prefix}fototiktok*
+┣⊱ *${prefix}map*
+┣⊱ *${prefix}kbbi*
+┣⊱ *${prefix}artinama*
+┣⊱ *${prefix}infogempa*
+┣⊱ *${prefix}quotes*
+┣⊱ *${prefix}cerpen*
+┣⊱ *${prefix}lirik*
+┣⊱ *${prefix}pokemon*
+┣⊱ *${prefix}chord*
+┣⊱ *${prefix}blocklist*
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}wolflogo*
+┣⊱ *${prefix}lionlogo*
+┣⊱ *${prefix}ssweb*
+┣⊱ *${prefix}anjing*
+┣⊱ *${prefix}indohot*
+┣━━❀ *Grupo* ❀━━
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}add* [+62xx]
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}demote* [@]
+┣⊱ *${prefix}promote* [@]
+┣⊱ *${prefix}kick* [@]
+┣⊱ *${prefix}listadmin* (Marca todos os adms no grupo)
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━❀ *Bot Proprietário* ❀━
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}block*
+┣⊱ *${prefix}unblock*
+┣⊱ *${prefix}bc*
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}setprefix*
+┣━━━━━━━━━━━━━━
+┃    *BARDOKILL DOMINA😡🇬🇧*
+┗━━━━━━━━━━━━━━
+
+`
+}
+exports.help = help
+
+
+
+  
+
